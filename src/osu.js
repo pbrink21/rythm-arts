d0ad6c3b4051f17f541dec896719a2070e45d93a
William <wament@iastate.edu>
2021-03-24 16:55:20 -0500

    created a few basic html pages with some css styling

---
+++ src/osu.js
@@ -0,0 +1,19 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+
+    }
+
+    
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    };
+}
+
+ReactDOM.render(<App />, document.getElementById("app"))
+//change this ^^
