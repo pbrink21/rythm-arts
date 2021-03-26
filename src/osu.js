af676bdfe9a27e55c8211d33cc0caf8c4a9884e5
pbrink21 <pbrink21@iastate.edu>
2021-03-26 12:33:58 -0500

    added basic clicking on canvas functionality

--- src/osu.js
+++ src/osu.js
@@ -1,18 +1,31 @@
 class App extends React.Component {
     constructor(props) {
         super(props);
-
-
+        this.state = {
+          circle: {
+            "x": 45,
+            "y": 50,
+            "width": 10,
+            "time": 10
+          }
+        }
     }
 
-    
+
 
     render() {
         return (
             <div>
+
             </div>
         );
     };
+
+    renderCanvas(){
+      return (
+
+      )
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById("app"))
