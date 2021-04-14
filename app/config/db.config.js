cec600463c78ac92a80d64a62714625f3bb7ec74
William <wament@iastate.edu>
2021-04-14 14:03:31 -0500

    backend api is working but only on localhost

---
+++ app/config/db.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // HOST: "coms-319-004.cs.iastate.edu",
+    // USER: "team1",
+    // PASSWORD: "team1comsVM@319",
+    // DB: "RhythmArts"
+
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "password",
+    DB: "rhythmarts"
+  };
--- server.js
+++ server.js
@@ -9,12 +9,7 @@ app.use(bodyParser.json());
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
-
-require("./app/routes/customer.routes.js")(app);
+require("./app/routes/user.routes.js")(app);
 
 // set port, listen for requests
 app.listen(3000, () => {
