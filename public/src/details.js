90dcbc08b88f4e8259a0787d9a4d2a82c5c5c4ca
Brett H <bhhaley@iastate.edu>
2021-04-28 02:07:04 -0500

    added points, perks, and user tracking on the shop page

---
+++ public/src/details.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+    displayOwnedPerks();
+  };
+
+function displayOwnedPerks(){
+    var user = getCookie("user");
+    var points = getCookie("points");
+    var saves = getCookie("saves");
+    var slows = getCookie("time");
+    var balls = getCookie("ball");
+
+    document.getElementById("numsaves").innerHTML = "More Saves Level: " + saves;
+    document.getElementById("numslows").innerHTML = "Slows Level: " + slows;
+    document.getElementById("numballs").innerHTML = "Balls Level: " + balls;
+    document.getElementById("pointsdisplay").innerHTML = "Points: " + points;
+    document.getElementById("usernamedisplay").innerHTML = "User: " + user;
+  }
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
